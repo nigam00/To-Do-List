cc840d16d9c003142da99a6844551ada4868dc3c
nigam00 <nigamagarwal5@gmail.com>
2017-12-30 18:09:12 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,27 @@
+//check off todo list by clicking
+$("ul").on("click","li",function(){
+	//if line is gray
+	$(this).toggleClass("completed");
+
+});
+//click on x to delete
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+
+});
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var todoText= $(this).val();
+		$(this).val("")
+		//create new li
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+todoText+"</li>");
+	}
+
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
